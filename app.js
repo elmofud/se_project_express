076b2323a915a268f650c9ca4e7651c72cea8e04
Tracey Lee <traceylg43@gmail.com>
2026-02-05 13:49:22 -0500

    fix syntax error and puncturation errors from npm run dev error'

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/users");
+
 const { PORT = 3001 } = process.env;
+const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
+
+app.use(express.json());
+app.use("/users", userRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
---
+++ controllers/users.js
@@ -0,0 +1,28 @@
+const User = require("../models/user");
+
+module.exports.getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.getUser = (req, res) => {
+  const { userId } = req.params;
+
+  User.findById(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      res.send({ data: user });
+    })
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+module.exports.createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
