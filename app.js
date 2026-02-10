aff8c53268be871c30c6833ae696ed2f9d0586d2
Tracey Lee <traceylg43@gmail.com>
2026-02-10 00:33:19 -0500

    refactor index and app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-// const userRoutes = require("./routes/users");
 const routes = require("./routes/index");
-const { ERROR_CODES, ERROR_MESSAGES } = require("./utils/errors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
