9502e06cdb6e4bab83fdfa4950dbeaf8c8c41e9d
Tracey Lee <traceylg43@gmail.com>
2026-02-21 17:30:47 -0500

    remove hard-code user ojbect

--- app.js
+++ app.js
@@ -9,13 +9,6 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 
 app.use(express.json());
 
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: "69860f1739e1df1363eca8db",
-//   };
-//   next();
-// });
-
 app.use(routes);
 
 app.listen(PORT, () => {
