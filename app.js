6a4ceb4e2d7f328ff12446b415cb49b1b2c75d93
Tracey Lee <traceylg43@gmail.com>
2026-02-22 09:39:40 -0500

    install and configure cors fix folder sytax

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes/index");
+const cors = require("cors");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
-
+app.use(cors());
 app.use(express.json());
 
 app.use(routes);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const userRoutes = require("./users");
 const clothingItemRoutes = require("./clothingItems");
 const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
 const { createUser, login } = require("../controllers/users");
-const auth = require("../middleware/auth");
+const auth = require("../middlewares/auth");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
