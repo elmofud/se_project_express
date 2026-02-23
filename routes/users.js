991587f256b345493cbd2caf064a73c7813db5be
Tracey Lee <traceylg43@gmail.com>
2026-02-23 11:31:45 -0500

    return createUser route

--- routes/users.js
+++ routes/users.js
@@ -3,11 +3,12 @@ const {
   getUsers,
   getCurrentUser,
   updateUser,
+  createUser,
 } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/me", getCurrentUser);
 
 router.patch("/me", updateUser);
-
+router.post("/", createUser);
 module.exports = router;
