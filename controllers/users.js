20ef3623156e6f3036f5c974646a37a2e773abfd
Tracey Lee <traceylg43@gmail.com>
2026-03-30 13:15:19 -0400

    Refactor controller users with centeralized error handling

--- controllers/users.js
+++ controllers/users.js
@@ -2,50 +2,40 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = require("../utils/config");
 const User = require("../models/user");
-const { ERROR_CODES, ERROR_MESSAGES } = require("../utils/errors");
+const { ERROR_MESSAGES } = require("../utils/errors");
+const NotFoundError = require("../errors/NotFoundError");
+const BadRequestError = require("../errors/BadRequestError");
+const ConflictError = require("../errors/ConflictError");
+const UnauthorizedError = require("../errors/unauthorizedError");
 
-module.exports.getCurrentUser = (req, res) => {
+module.exports.getCurrentUser = (req, res, next) => {
   const { _id } = req.user;
 
   User.findById(_id)
     .orFail(() => {
-      const error = new Error(ERROR_MESSAGES.USER_NOT_FOUND);
-      error.statusCode = ERROR_CODES.NOT_FOUND;
-      throw error;
+      throw new NotFoundError(ERROR_MESSAGES.USER_NOT_FOUND);
     })
     .then((user) => {
       res.send({ data: user });
     })
     .catch((err) => {
-      console.error(err);
       if (err.name === "CastError") {
-        return res
-          .status(ERROR_CODES.BAD_REQUEST)
-          .send({ message: ERROR_MESSAGES.INVALID_ID });
+        next(new BadRequestError(ERROR_MESSAGES.INVALID_ID));
+      } else {
+        next(err);
       }
-      if (err.statusCode === ERROR_CODES.NOT_FOUND) {
-        return res
-          .status(ERROR_CODES.NOT_FOUND)
-          .send({ message: ERROR_MESSAGES.USER_NOT_FOUND });
-      }
-      return res
-        .status(ERROR_CODES.DEFAULT_ERROR)
-        .send({ message: ERROR_MESSAGES.SERVER_ERROR });
     });
 };
 
-module.exports.createUser = (req, res) => {
+module.exports.createUser = (req, res, next) => {
   const { name, avatar, email, password } = req.body;
-  if (password.length < 8) {
-    return res
-      .status(ERROR_CODES.BAD_REQUEST)
-      .send({ message: ERROR_MESSAGES.PASSWORD_TO_SHORT });
-  }
   if (!password || typeof password !== "string") {
-    return res
-      .status(ERROR_CODES.BAD_REQUEST)
-      .json({ message: ERROR_MESSAGES.PASSWORD_REQUIRED_STRING });
+    return next(new BadRequestError(ERROR_MESSAGES.PASSWORD_REQUIRED_STRING));
   }
+  if (password.length < 8) {
+    return next(new BadRequestError(ERROR_MESSAGES.PASSWORD_TO_SHORT));
+  }
+
   return bcrypt
     .hash(password, 10)
     .then((hashedPassword) =>
@@ -57,30 +47,21 @@ module.exports.createUser = (req, res) => {
       return res.status(201).send({ data: userObject });
     })
     .catch((err) => {
-      console.error(err);
       if (err.name === "ValidationError") {
-        return res
-          .status(ERROR_CODES.BAD_REQUEST)
-          .send({ message: ERROR_MESSAGES.INVALID_DATA });
+        next(new BadRequestError(ERROR_MESSAGES.INVALID_DATA));
+      } else if (err.code === 11000) {
+        next(new ConflictError(ERROR_MESSAGES.CONFLICTED_DATA));
+      } else {
+        next(err);
       }
-      if (err.code === 11000) {
-        return res
-          .status(ERROR_CODES.CONFLICT_DATA)
-          .send({ message: ERROR_MESSAGES.CONFLICTED_DATA });
-      }
-      return res
-        .status(ERROR_CODES.DEFAULT_ERROR)
-        .send({ message: ERROR_MESSAGES.SERVER_ERROR });
     });
 };
 
-module.exports.login = (req, res) => {
+module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
-    return res
-      .status(ERROR_CODES.BAD_REQUEST)
-      .send({ message: ERROR_MESSAGES.INVALID_DATA });
+    return next(new BadRequestError(ERROR_MESSAGES.INVALID_DATA));
   }
 
   return User.findUserByCredentials(email, password)
@@ -91,20 +72,15 @@ module.exports.login = (req, res) => {
       return res.send({ token });
     })
     .catch((err) => {
-      console.error(err);
       if (err.message === ERROR_MESSAGES.UNAUTHORIZED_EMAIL_PASSWORD) {
-        return res
-          .status(ERROR_CODES.UNAUTHORIZED)
-          .send({ message: ERROR_MESSAGES.UNAUTHORIZED_EMAIL_PASSWORD });
+        next(new UnauthorizedError(ERROR_MESSAGES.UNAUTHORIZED_EMAIL_PASSWORD));
+      } else {
+        next(err);
       }
-
-      return res
-        .status(ERROR_CODES.DEFAULT_ERROR)
-        .send({ message: ERROR_MESSAGES.SERVER_ERROR });
     });
 };
 
-module.exports.updateUser = (req, res) => {
+module.exports.updateUser = (req, res, next) => {
   const { name, avatar } = req.body;
   const { _id } = req.user;
   User.findByIdAndUpdate(
@@ -116,25 +92,14 @@ module.exports.updateUser = (req, res) => {
     }
   )
     .orFail(() => {
-      const error = new Error(ERROR_MESSAGES.USER_NOT_FOUND);
-      error.statusCode = ERROR_CODES.NOT_FOUND;
-      throw error;
+      throw new NotFoundError(ERROR_MESSAGES.USER_NOT_FOUND);
     })
     .then((user) => res.send({ data: user }))
     .catch((err) => {
-      console.error(err);
       if (err.name === "ValidationError") {
-        return res
-          .status(ERROR_CODES.BAD_REQUEST)
-          .send({ message: ERROR_MESSAGES.INVALID_DATA });
+        next(new BadRequestError(ERROR_MESSAGES.INVALID_DATA));
+      } else {
+        next(err);
       }
-      if (err.statusCode === ERROR_CODES.NOT_FOUND) {
-        return res
-          .status(ERROR_CODES.NOT_FOUND)
-          .send({ message: ERROR_MESSAGES.USER_NOT_FOUND });
-      }
-      return res
-        .status(ERROR_CODES.DEFAULT_ERROR)
-        .send({ message: ERROR_MESSAGES.SERVER_ERROR });
     });
 };
