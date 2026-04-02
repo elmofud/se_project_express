02f21b45ff47a2bbaf874a394f1afd3be0c02c6a
Tracey Lee <traceylg43@gmail.com>
2026-04-02 01:20:34 -0400

    Add request and error logger middlewares

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { errors } = require("celebrate");
 const routes = require("./routes/index");
 const errorHandler = require("./middlewares/error-handler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -12,10 +13,12 @@ mongoose.connect("mongodb://127.0.0.1:27017/wtwr_db");
 app.use(cors());
 app.use(express.json());
 
+app.use(requestLogger);
 app.use(routes);
 
-app.use(errors());
+app.use(errorLogger);
 
+app.use(errors());
 app.use(errorHandler);
 
 app.listen(PORT, () => {
